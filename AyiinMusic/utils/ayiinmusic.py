82f054d970f16d79ad37d83cfa9624f2302bb807
YinsXd <100098474+YinsXd@users.noreply.github.com>
2022-05-09 18:51:07 +0800

    Update ayiinmusic.py

--- AyiinMusic/utils/ayiinmusic.py
+++ AyiinMusic/utils/ayiinmusic.py
@@ -2,7 +2,6 @@ from pyrogram.errors import ChatAdminRequired, ChatWriteForbidden, UserNotPartic
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from AyiinMusic import app
-import config
 
 
 def ayiin(func):
@@ -10,20 +9,21 @@ def ayiin(func):
         user_id = message.from_user.id
         user_name = message.from_user.first_name
         ppk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-        if not config.MUST_JOIN:  # Not compulsory
+        MUST_JOIN = "https://t.me/AyiinXdSupport"
+        if not MUST_JOIN:  # Not compulsory
             return
         try:
             try:
-                await app.get_chat_member(config.MUST_JOIN, message.from_user.id)
+                await app.get_chat_member(MUST_JOIN, message.from_user.id)
             except UserNotParticipant:
-                if config.MUST_JOIN.isalpha():
-                    link = "https://t.me/" + config.MUST_JOIN
+                if MUST_JOIN.isalpha():
+                    link = MUST_JOIN
                 else:
-                    chat_info = await app.get_chat(config.MUST_JOIN)
+                    chat_info = await app.get_chat(MUST_JOIN)
                     chat_info.invite_link
                 try:
                     await message.reply(
-                        f"**Hay kak {ppk}, Silahkan join dulu biar bisa pake bot ini kak**",
+                        f"**Hay {ppk}, Silahkan Join Untuk Menggunakan Saya**",
                         disable_web_page_preview=True,
                         reply_markup=InlineKeyboardMarkup(
                             [[InlineKeyboardButton("⍟ ᴊᴏɪɴ ᴛᴏᴅ ⍟", url=link)]]
@@ -34,7 +34,7 @@ def ayiin(func):
                     pass
         except ChatAdminRequired:
             await message.reply(
-                f"Saya bukan admin di chat MUST_JOIN chat : {config.MUST_JOIN} !"
+                f"Saya bukan admin di chat AyiinSupport !"
             )
         return await func(_, message)
 
